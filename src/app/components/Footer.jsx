17870ef9987801b31142083a2cf0f60834826425
Christine Chee <christinechee2000@gmail.com>
2026-02-13 14:12:05 -0500

    footer style fixes

--- src/app/components/Footer.jsx
+++ src/app/components/Footer.jsx
@@ -4,56 +4,56 @@ import Link from 'next/link';
 
 export default function Footer() {
     return (
-        <footer className="w-full bg-[var(--grey-black)] text-white">
+        <footer className='w-full bg-[var(--grey-black)] text-white'>
             <div
-                className="grid grid-cols-1 md:grid-cols-3 gap-4 p-6
-                max-w-[1300px] mx-auto text-center md:text-left"
+                className='grid grid-cols-1 md:grid-cols-3 gap-4 p-6
+                max-w-[1300px] mx-auto text-center md:text-left'
             >
                 {/* Logo */}
                 <Link
-                href="/"
-                className="flex flex-col justify-start items-center"
+                href='/'
+                className='flex flex-col justify-start items-center'
                 >
                     <img
-                        src="/wabisabi_logo_white.png"
-                        alt="WABI SABI jp"
-                        className="w-52 object-cover"
+                        src='/wabisabi_logo_white.png'
+                        alt='WABI SABI jp'
+                        className='w-52 object-cover'
                     />
                 </Link>
 
                 {/* Pages */}
-                <div className="flex flex-col justify-start items-center gap-2 p-4">
-                    <h2 className="uppercase font-semibold tracking-wider">
+                <div className='flex flex-col justify-start items-center gap-2 p-4'>
+                    <h2 className='uppercase font-semibold tracking-wider'>
                         - Pages -
                     </h2>
 
-                    <LinkButton href="/" className="text-white hover:text-white">Home</LinkButton>
-                    <LinkButton href="/about" className="text-white hover:text-white">About</LinkButton>
-                    <LinkButton href="/products" className="text-white hover:text-white">Products</LinkButton>
-                    <LinkButton href="/contact" className="text-white hover:text-white">Contact</LinkButton>
+                    <LinkButton href='/' className='text-white hover:text-white'>Home</LinkButton>
+                    <LinkButton href='/about' className='text-white hover:text-white'>About</LinkButton>
+                    <LinkButton href='/products' className='text-white hover:text-white'>Products</LinkButton>
+                    <LinkButton href='/contact' className='text-white hover:text-white'>Contact</LinkButton>
                 </div>
 
                 {/* Socials */}
-                <div className="flex flex-col justify-start items-center gap-2 pt-4">
-                    <h2 className="uppercase font-medium tracking-wider">
+                <div className='flex flex-col justify-start items-center gap-2 pt-4'>
+                    <h2 className='uppercase font-medium tracking-wider'>
                         - Socials -
                     </h2>
 
                     <Link
-                        href="https://www.instagram.com/"
-                        target="_blank"
-                        className="inline-block transition-all duration-300 hover:scale-110"
+                        href='https://www.instagram.com/'
+                        target='_blank'
+                        className='inline-block transition-all duration-300 hover:scale-110'
                     >
                         <img
-                        src="/Instagram.png"
-                        alt="Instagram"
-                        className="h-12 w-auto"
+                        src='/Instagram.png'
+                        alt='Instagram'
+                        className='h-12 w-auto'
                         />
                     </Link>
                 </div>
 
                 {/* Bottom row */}
-                <p className="col-span-1 md:col-span-3 text-center pt-4">
+                <p className='col-span-1 md:col-span-3 text-center pt-4'>
                     &copy; WabiSabi.jp
                 </p>
             </div>
